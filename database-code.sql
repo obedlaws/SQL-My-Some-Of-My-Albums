aac00f2f7e8ee6df9996b99598e60143cc28ee6f
Obed Laws <obedlaws@outlook.com>
2022-02-14 20:51:03 -0500

    Update database-code.sql

--- database-code.sql
+++ database-code.sql
@@ -1,18 +1,18 @@
 CREATE TABLE public.albums (
 	album_name VARCHAR(50),
-  	artist_or_band VARCHAR(50) PRIMARY KEY,
+  	artist_or_band VARCHAR(50) UNIQUE,
 	release_date DATE,
-	publishing_label VARCHAR(50) PRIMARY KEY
+	publishing_label VARCHAR(50) UNIQUE
 );
 
-CREATE TABLE public.labels (
-	label_name VARCHAR(50) FOREIGN KEY REFERENCES albums(publishing_label),
+CREATE TABLE public.publishing_labels (
+	name VARCHAR(50) REFERENCES albums (publishing_label),
   	foundation_date DATE,
 	main_genre VARCHAR(50)
 );
 
 CREATE TABLE public.artistband (
-	name VARCHAR(50) FOREIGN KEY REFERENCES albums(),
+	name VARCHAR(50) REFERENCES albums (artist_or_band),
 	genre VARCHAR(50),
 	total_album_sales_copies int
 );
